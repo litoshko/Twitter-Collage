992f0f8505050bd89d7c6a9e6001c3bc99c40d3d
xxoro <xxoro@mail.ru>
2015-09-13 22:45:56 +0300

    add home controller, index view

---
+++ web twitter collage/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_twitter_collage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string twitterUserName)
+        {
+            ViewBag.userName = twitterUserName;
+
+            return View();
+        }
+    }
+
+    public class TwitterCollage
+    {
+
+    }
+}
